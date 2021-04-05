27ea6265b4edc6b3a42182339b2b8f1486ce8f43
Matthew Vanni <mvanni4994@gmail.com>
2021-04-05 03:37:46 -0400

    added database code to store transactions

--- public/database.js
+++ public/database.js
@@ -1,15 +1,16 @@
-let database;
+let db;
 const request = indexDatabase.open("budget", 1);
 
 request.onupgradeneeded = function(event){
-    const database = event.target.result;
-    database.createObjectStore("pending", {autoIncrement: true});
+    const db = event.target.result;
+    db.createObjectStore("pending", {autoIncrement: true});
 };
 
 request.onsuccess = function(event) {
-    database = event.target.result;
+    db = event.target.result;
 
     // check app status before pulling from database
+
 if(navigator.onLine){
     checkDatabase();
 }
@@ -18,3 +19,41 @@ if(navigator.onLine){
 request.onerror = function(event) {
     console.log("Error" + event.targer.errorCode);
 };
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending"), 
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function(){
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                header: {
+                            // define content type the client accepts
+                    accept: "application/json, text/plain, */*",
+                    "Content-Type":"application/json",
+                },
+            })
+            .then((response) => response.json())
+            .then(() => {
+                const transfer = db.transaction(["pending"], "readwrite");
+                const store = transfer.objectStore("pending");
+                // clear all stored in database
+                store.clear();
+            });
+        }
+    };
+}
+
+function saveTransaction(record) {
+    const transaction = dbtransaction(["pending"], "readwrite");
+    const store = transaction.objectStore(pending);
+    // record & save transaction for future
+    store.add(record)
+
+}
+
+window.addEventListener("online", checkDatabase)
