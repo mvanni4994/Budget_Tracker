6dfd49034978b4e23fbc0b258fd47c5805df45df
Matthew Vanni <mvanni4994@gmail.com>
2021-04-05 04:06:40 -0400

    added service workers for offline storage and returns cache when online

---
+++ public/service-worker.js
@@ -0,0 +1,51 @@
+const cacheFiles= ["./", "./index.html", "./index.js", "./styles.css"];
+const cacheName = "Static Cache"
+const dataCache = "Data Cache"
+
+self.addEventListener("install", function(e){
+    e.waitUntil(
+        catches.open(cacheName).then(cache => {
+            console.log("saved successfully");
+            return cache.addAll(cacheFiles);
+        })
+    
+);
+
+self.skipWaiting();
+    });
+
+// fetch data
+self.addEventListener("fetch", function(e){
+    if(e.request.url.include("/api/")){
+        e.respondWith(
+            caches.open(dataCache).then(cache => {
+                return fetch(e.request)
+                .then(response => {
+                    // store response
+                    if (response.status === 200) {
+                        cache.put(e.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    // fetch from cache if no connection is available
+                    return cache.match(e.request);
+                });
+            })
+            .cache(err => console.log(err))
+        );
+        return;
+    }
+    e.respondWith(
+        fetch(e.request).catch(function(){
+            return caches.match(e.request).then(function(response){
+                if(response){
+                    return response;
+                }
+                else if (e.request.headers.get("accept").includes("text/html")) {
+                    return caches.match("/");
+                }
+            });
+        })
+    );
+});
